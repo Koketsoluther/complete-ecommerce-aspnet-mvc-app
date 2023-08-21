3e10d5d6885592fd1366e7da7f165d271a37be78
koket <koket@LAPTOP-QPCPCUG9>
2023-08-21 17:08:31 +0200

    Add project files.

---
+++ CropTrakWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CropTrakWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
